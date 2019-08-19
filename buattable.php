74f7eca34db4849092ff8170f08ebae6fe9cd742
meizano <meizanoam@gmail.com>
2019-08-19 23:31:39 +0700

    BUGFIX: Menghapus petik (`)

--- buattable.php
+++ buattable.php
@@ -14,19 +14,19 @@ try {
     -- --------------------------------------------------------
 
     --
-    -- Table structure for table `kategori`
+    -- Table structure for table kategori
     --
 
-    CREATE TABLE IF NOT EXISTS `kategori` (
-      `id_kategori` int(11) NOT NULL,
-      `kategori` varchar(25) NOT NULL
+    CREATE TABLE IF NOT EXISTS kategori (
+      id_kategori int(11) NOT NULL,
+      kategori varchar(25) NOT NULL
     ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
     --
-    -- Dumping data for table `kategori`
+    -- Dumping data for table kategori
     --
 
-    INSERT INTO `kategori` (`kategori`) VALUES
+    INSERT INTO kategori (kategori) VALUES
     ('umum'),
     ('fasilitas'),
     ('peminjaman'),
@@ -37,35 +37,35 @@ try {
     -- --------------------------------------------------------
 
     --
-    -- Table structure for table `pertanyaan`
+    -- Table structure for table pertanyaan
     --
 
-    CREATE TABLE IF NOT EXISTS `pertanyaan` (
-      `id_pertanyaan` int(11) NOT NULL,
-      `id_survey` int(11) NOT NULL,
-      `q1` int(11) DEFAULT NULL,
-      `q2` int(11) DEFAULT NULL,
-      `q3` int(11) DEFAULT NULL,
-      `q4` int(11) DEFAULT NULL,
-      `q5` int(11) DEFAULT NULL,
-      `q6` int(11) DEFAULT NULL,
-      `q7` int(11) DEFAULT NULL,
-      `q8` int(11) DEFAULT NULL,
-      `q9` int(11) DEFAULT NULL,
-      `q10` int(11) DEFAULT NULL
+    CREATE TABLE IF NOT EXISTS pertanyaan (
+      id_pertanyaan int(11) NOT NULL,
+      id_survey int(11) NOT NULL,
+      q1 int(11) DEFAULT NULL,
+      q2 int(11) DEFAULT NULL,
+      q3 int(11) DEFAULT NULL,
+      q4 int(11) DEFAULT NULL,
+      q5 int(11) DEFAULT NULL,
+      q6 int(11) DEFAULT NULL,
+      q7 int(11) DEFAULT NULL,
+      q8 int(11) DEFAULT NULL,
+      q9 int(11) DEFAULT NULL,
+      q10 int(11) DEFAULT NULL
     ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
     -- --------------------------------------------------------
 
     --
-    -- Table structure for table `survey`
+    -- Table structure for table survey
     --
 
-    CREATE TABLE IF NOT EXISTS `survey` (
-      `id_survey` int(11) NOT NULL,
-      `id_kategori` int(11) NOT NULL,
-      `id_mesin` int(11) NOT NULL,
-      `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    CREATE TABLE IF NOT EXISTS survey (
+      id_survey int(11) NOT NULL,
+      id_kategori int(11) NOT NULL,
+      id_mesin int(11) NOT NULL,
+      waktu timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
     ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
     --
@@ -73,57 +73,57 @@ try {
     --
 
     --
-    -- Indexes for table `kategori`
+    -- Indexes for table kategori
     --
-    ALTER TABLE `kategori`
-      ADD PRIMARY KEY (`id_kategori`), ADD UNIQUE KEY `kategori` (`kategori`);
+    ALTER TABLE kategori
+      ADD PRIMARY KEY (id_kategori), ADD UNIQUE KEY kategori (kategori);
 
     --
-    -- Indexes for table `pertanyaan`
+    -- Indexes for table pertanyaan
     --
-    ALTER TABLE `pertanyaan`
-      ADD PRIMARY KEY (`id_pertanyaan`), ADD UNIQUE KEY `id_survey_2` (`id_survey`), ADD KEY `id_survey` (`id_survey`);
+    ALTER TABLE pertanyaan
+      ADD PRIMARY KEY (id_pertanyaan), ADD UNIQUE KEY id_survey_2 (id_survey), ADD KEY id_survey (id_survey);
 
     --
-    -- Indexes for table `survey`
+    -- Indexes for table survey
     --
-    ALTER TABLE `survey`
-      ADD PRIMARY KEY (`id_survey`), ADD KEY `id_kategori` (`id_kategori`), ADD KEY `id_mesin` (`id_mesin`);
+    ALTER TABLE survey
+      ADD PRIMARY KEY (id_survey), ADD KEY id_kategori (id_kategori), ADD KEY id_mesin (id_mesin);
 
     --
     -- AUTO_INCREMENT for dumped tables
     --
 
     --
-    -- AUTO_INCREMENT for table `kategori`
+    -- AUTO_INCREMENT for table kategori
     --
-    ALTER TABLE `kategori`
-      MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+    ALTER TABLE kategori
+      MODIFY id_kategori int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
     --
-    -- AUTO_INCREMENT for table `pertanyaan`
+    -- AUTO_INCREMENT for table pertanyaan
     --
-    ALTER TABLE `pertanyaan`
-      MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+    ALTER TABLE pertanyaan
+      MODIFY id_pertanyaan int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
     --
-    -- AUTO_INCREMENT for table `survey`
+    -- AUTO_INCREMENT for table survey
     --
-    ALTER TABLE `survey`
-      MODIFY `id_survey` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+    ALTER TABLE survey
+      MODIFY id_survey int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
     --
     -- Constraints for dumped tables
     --
 
     --
-    -- Constraints for table `pertanyaan`
+    -- Constraints for table pertanyaan
     --
-    ALTER TABLE `pertanyaan`
-    ADD CONSTRAINT `fk_survey` FOREIGN KEY (`id_survey`) REFERENCES `survey` (`id_survey`);
+    ALTER TABLE pertanyaan
+    ADD CONSTRAINT fk_survey FOREIGN KEY (id_survey) REFERENCES survey (id_survey);
 
     --
-    -- Constraints for table `survey`
+    -- Constraints for table survey
     --
-    ALTER TABLE `survey`
-    ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+    ALTER TABLE survey
+    ADD CONSTRAINT fk_kategori FOREIGN KEY (id_kategori) REFERENCES kategori (id_kategori);
     ");
 
     $handle->execute();
