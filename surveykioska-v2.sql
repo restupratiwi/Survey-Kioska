1d3e426a23daa0448f72cca3d67b2bd96e763891
Restu Pratiwi <restupratiwi1603@gmail.com>
2019-08-20 12:20:28 +0700

    Update surveykioska-v2.sql

--- surveykioska-v2.sql
+++ surveykioska-v2.sql
@@ -1,23 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2019 at 06:20 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Aug 14, 2019 at 10:13 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `surveykioska`
+-- Database: `survey`
 --
 
 -- --------------------------------------------------------
@@ -26,22 +28,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `kategori`
 --
 
-CREATE TABLE IF NOT EXISTS `kategori` (
+CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
   `kategori` varchar(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`kategori`) VALUES
-('umum'),
-('fasilitas'),
-('peminjaman'),
-('kioska navigasi'),
-('navigasi ar'),
-('vr tour');
+INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
+(0, 'umum'),
+(0, 'fasilitas'),
+(0, 'peminjaman'),
+(0, 'kioska navigasi'),
+(0, 'navigasi ar'),
+(0, 'vr tour');
 
 -- --------------------------------------------------------
 
@@ -49,7 +51,7 @@ INSERT INTO `kategori` (`kategori`) VALUES
 -- Table structure for table `pertanyaan`
 --
 
-CREATE TABLE IF NOT EXISTS `pertanyaan` (
+CREATE TABLE `pertanyaan` (
   `id_pertanyaan` int(11) NOT NULL,
   `id_survey` int(11) NOT NULL,
   `q1` int(11) DEFAULT NULL,
@@ -62,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `pertanyaan` (
   `q8` int(11) DEFAULT NULL,
   `q9` int(11) DEFAULT NULL,
   `q10` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -70,69 +72,13 @@ CREATE TABLE IF NOT EXISTS `pertanyaan` (
 -- Table structure for table `survey`
 --
 
-CREATE TABLE IF NOT EXISTS `survey` (
+CREATE TABLE `survey` (
   `id_survey` int(11) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `id_mesin` int(11) NOT NULL,
-  `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `kategori`
---
-ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id_kategori`), ADD UNIQUE KEY `kategori` (`kategori`);
-
---
--- Indexes for table `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  ADD PRIMARY KEY (`id_pertanyaan`), ADD UNIQUE KEY `id_survey_2` (`id_survey`), ADD KEY `id_survey` (`id_survey`);
-
---
--- Indexes for table `survey`
---
-ALTER TABLE `survey`
-  ADD PRIMARY KEY (`id_survey`), ADD KEY `id_kategori` (`id_kategori`), ADD KEY `id_mesin` (`id_mesin`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `kategori`
---
-ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `survey`
---
-ALTER TABLE `survey`
-  MODIFY `id_survey` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-ADD CONSTRAINT `fk_survey` FOREIGN KEY (`id_survey`) REFERENCES `survey` (`id_survey`);
-
---
--- Constraints for table `survey`
---
-ALTER TABLE `survey`
-ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+  `waktu` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
